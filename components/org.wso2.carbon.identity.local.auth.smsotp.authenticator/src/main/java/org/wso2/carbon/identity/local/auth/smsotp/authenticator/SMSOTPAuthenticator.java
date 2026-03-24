34dccc34141b3650eac7016833910d75fc55f397
shanjayathilaka <shanchathusanda@gmail.com>
2026-03-24 11:21:41 +0530

    Get SMS Provider configurations using the inherited configurations

--- components/org.wso2.carbon.identity.local.auth.smsotp.authenticator/src/main/java/org/wso2/carbon/identity/local/auth/smsotp/authenticator/SMSOTPAuthenticator.java
+++ components/org.wso2.carbon.identity.local.auth.smsotp.authenticator/src/main/java/org/wso2/carbon/identity/local/auth/smsotp/authenticator/SMSOTPAuthenticator.java
@@ -580,7 +580,7 @@ public class SMSOTPAuthenticator extends AbstractOTPAuthenticator implements Loc
 
         try {
             Resource resource = AuthenticatorDataHolder.getConfigurationManager().getResource(SMSOTPConstants.PUBLISHER,
-                    SMSOTPConstants.SMS_PROVIDER);
+                    SMSOTPConstants.SMS_PROVIDER, true);
             if (resource != null) {
                 return SMSOTPConstants.ProviderTypes.CUSTOM;
             }
